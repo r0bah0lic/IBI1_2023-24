ff1991be533c320c04d9b69a2fd84fd68e870553
misaki <misaki@misakideMacBook-Air.local>
2023-03-15 14:47:49 +0800

    a

--- Practical5/hexagons.py
+++ Practical5/hexagons.py
@@ -1,16 +1,5 @@
-n = 1
-h = n*(2*n-1)
-print(h)
-a = 2
-b = a*(2*a-1)
-print(b)
-c = 3
-d = c*(2*c-1)
-print(d)
-e = 4
-f = e*(2*e-1)
-print(f)
-g = 5
-j = g*(2*g-1)
-print(j)
+for n in range(1,6):
+  print(n, "is", n*(2*n-1))
+
+
 
