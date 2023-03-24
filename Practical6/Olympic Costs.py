b46fff198e038610d43a9bf0f2858dedb796fdb1
MYan <127280353+MYanMisaki@users.noreply.github.com>
2023-03-24 23:20:18 +0800

    Update Olympic Costs.py

--- Practical6/Olympic Costs.py
+++ Practical6/Olympic Costs.py
@@ -1,9 +1,11 @@
-#list the costs first
+#Enter the costs and cities
+#link the costs and cities together
 #then store it of sorted values
 #print the sorted list
 costs = [1,8,15,7,5,14,43,40]
-costs.sort()
-print(costs)
+cities = ['Los Angeles 1984','Sydney 2000','Atlanta 1996','Seoul 1988','Athens 2003','Barcelona 1992','London 2012','Beijing 2008']
+sorted_values=sorted(costs)
+print(sorted_values)
 
 #import the matplotlib first
 #Initializes shortcuts to graph and subgraph axes
@@ -11,8 +13,8 @@ print(costs)
 #set the color of the bars
 #set the title of the diagram
 #show the picture
-
-cities = ['Los Angeles 1984','Sydney 2000','Atlanta 1996','Seoul 1988','Athens 2003','Barcelona 1992','London 2012','Beijing 2008']
+import numpy
+print(costs)
 import numpy
 cities=numpy.array(cities)
 costs=numpy.array(costs)
@@ -25,8 +27,8 @@ fig, ax = plt.subplots()
 plt.xlabel("cities")
 plt.ylabel("costs")
 bar_colors=['tab:blue','tab:blue','tab:blue','tab:blue','tab:blue','tab:blue','tab:blue','tab:blue']
-ax.bar(cities,costs, color=bar_colors)
+width=0.55
+ax.bar(sorted_cities,sorted_costs, color=bar_colors)
 ax.set_ylabel('Cost (in $ billions)')
 ax.set_title('Olympic Costs')  
 plt.show()
-
