b85cef4dc92e61b2162705bb98dc67e65fa052a2
MYan <127280353+MYanMisaki@users.noreply.github.com>
2023-04-14 16:12:48 +0800

    Update cds_counter.py

--- Practical9/cds_counter.py
+++ Practical9/cds_counter.py
@@ -1,3 +1,11 @@
+#automatically count the total number of possible coding sequences formed by this sequence
+#first input the seq
+#find the begins ATG
+#count the number of TGA, let it be a
+#count the number of TAA, let it be b
+#add a and b
+
+
 seq = 'ATGCAATCGACTACGATCTGAGAGGGCCTAA'
 l = seq.find('ATG')
 a = seq.count('TGA',l)
