d474baec6d1ad4c834dd9f0f376c31bfe33e2e8d
MYan <127280353+MYanMisaki@users.noreply.github.com>
2023-04-19 15:20:03 +0800

    Create 1Protein-coding capability calculator.py

---
+++ Practical10/1Protein-coding capability calculator.py
@@ -0,0 +1,37 @@
+#Here is an example
+seq = 'AAAAATGCCCTAG'
+
+str = seq
+class calculator(object):
+    def __init__(self,total_DNA, target_DNA):
+        self.total = total_DNA
+        self.target = target_DNA
+    def count(self):
+        if self.target > 0.5*self.total:
+            print('protein-coding')
+        elif self.target < 0.1*self.total:
+            print('non-coding')
+        else:
+            print('unclear')
+DNA = calculator(len(str),str.index('TAG') - str.index('ATG') + 3)
+DNA.count()
+
+
+#here you can input whatever you like
+print("\nhere you can try a new DNA sequence")
+seq = input('DNA sequence:')
+
+str = seq
+class calculator(object):
+    def __init__(self,total_DNA, target_DNA):
+        self.total = total_DNA
+        self.target = target_DNA
+    def count(self):
+        if self.target > 0.5*self.total:
+            print('protein-coding')
+        elif self.target < 0.1*self.total:
+            print('non-coding')
+        else:
+            print('unclear')
+DNA = calculator(len(str),str.index('TAG') - str.index('ATG') + 3)
+DNA.count()
