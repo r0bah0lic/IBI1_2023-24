2e2bcc2c23f46ddbfd3b0def9e7b7b8a3cbdd74c
MYan <127280353+MYanMisaki@users.noreply.github.com>
2023-04-19 15:30:44 +0800

    Update and rename Protein-coding capability calculator.py to Mortgage affordability calculator.py

--- Practical10/Mortgage affordability calculator.py
+++ Practical10/Mortgage affordability calculator.py
@@ -1,3 +1,5 @@
+#Create a class
+#Set two parameters 'value' and 'salary'
 #here is an example, I assume that the value is 30 million dollars and my salary is 7 million dollars.
 print("if I have 7 million dollars, and the value is 30 million dollars")
 class calculator(object):
